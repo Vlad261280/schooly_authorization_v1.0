7da3cea8e152fef000a2a599ade12bcee6612123
ttyomaa6 <81015461+ttyomaa6@users.noreply.github.com>
2021-06-19 17:32:02 +0300

    enter with google play

--- app/src/main/java/com/egormoroz/schooly/MainActivity.java
+++ app/src/main/java/com/egormoroz/schooly/MainActivity.java
@@ -1,22 +1,18 @@
 package com.egormoroz.schooly;
 
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
+import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentTransaction;
+
 import android.annotation.SuppressLint;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
-import android.view.View;
 import android.widget.TextView;
 
-import androidx.annotation.NonNull;
-import androidx.appcompat.app.AppCompatActivity;
-import androidx.appcompat.widget.Toolbar;
-import androidx.coordinatorlayout.widget.CoordinatorLayout;
-import androidx.fragment.app.Fragment;
-import androidx.fragment.app.FragmentTransaction;
-
 import com.egormoroz.schooly.ui.main.MainFragment;
-import com.egormoroz.schooly.ui.main.MessengerFragment;
-import com.egormoroz.schooly.ui.main.NontificationFragment;
 import com.egormoroz.schooly.ui.news.NewsFragment;
 import com.egormoroz.schooly.ui.people.PeopleFragment;
 import com.egormoroz.schooly.ui.profile.ProfileFragment;
@@ -31,33 +27,7 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
 
         Toolbar toolbar = findViewById(R.id.toolbar);
-        AppBarLayout appBarLayout = findViewById(R.id.AppBarLayout);
         setSupportActionBar(toolbar);
-
-
-        CoordinatorLayout fragmentContainer = findViewById(R.id.fragment_container);
-
-        toolbar.setNavigationOnClickListener(new View.OnClickListener() { // notifications
-            @Override
-            public void onClick(View v) {
-                setCurrentFragment(NontificationFragment.newInstance());
-                CoordinatorLayout.LayoutParams coordinatorLayoutParams = (CoordinatorLayout.LayoutParams) fragmentContainer.getLayoutParams();
-                coordinatorLayoutParams.setBehavior(null);
-            }
-        });
-
-        toolbar.setOnMenuItemClickListener(new Toolbar.OnMenuItemClickListener() {
-            @SuppressLint("NonConstantResourceId")
-            @Override
-            public boolean onMenuItemClick(MenuItem item) {
-                if (item.getItemId() == R.id.nav_top_messages) {
-                    setCurrentFragment(MessengerFragment.newInstance());
-                    CoordinatorLayout.LayoutParams coordinatorLayoutParams = (CoordinatorLayout.LayoutParams) fragmentContainer.getLayoutParams();
-                    coordinatorLayoutParams.setBehavior(null);
-                }
-                return false;
-            }
-        });
 
         TextView toolbarTitle = findViewById(R.id.toolbar_title);
 
@@ -75,25 +45,19 @@ public class MainActivity extends AppCompatActivity {
                         setCurrentFragment(MainFragment.newInstance());
                         toolbarTitle.setText(getString(R.string.app_name));
                         toolbarTitle.setTextColor(getColor(R.color.purple_300));
-                        appBarLayout.setVisibility(View.VISIBLE);
                         return true;
                     case R.id.bottom_nav_news:
                         setCurrentFragment(NewsFragment.newInstance());
                         toolbarTitle.setText(getString(R.string.toolbar_news));
                         toolbarTitle.setTextColor(getColor(R.color.black));
-                        appBarLayout.setVisibility(View.VISIBLE);
                         return true;
                     case R.id.bottom_nav_people:
                         setCurrentFragment(PeopleFragment.newInstance());
                         toolbarTitle.setText(getString(R.string.toolbar_people));
                         toolbarTitle.setTextColor(getColor(R.color.black));
-                        appBarLayout.setVisibility(View.VISIBLE);
                         return true;
                     case R.id.bottom_nav_profile:
                         setCurrentFragment(ProfileFragment.newInstance());
-                        appBarLayout.setVisibility(View.GONE);
-                        CoordinatorLayout.LayoutParams coordinatorLayoutParams = (CoordinatorLayout.LayoutParams) fragmentContainer.getLayoutParams();
-                        coordinatorLayoutParams.setBehavior(null);
                         return true;
                 }
                 return false;
@@ -107,7 +71,7 @@ public class MainActivity extends AppCompatActivity {
         return true;
     }
 
-    public void setCurrentFragment(Fragment fragment) {
+    private void setCurrentFragment(Fragment fragment) {
         FragmentTransaction ft = getSupportFragmentManager().beginTransaction();
         ft.replace(R.id.frame, fragment);
         ft.commit();
--- app/src/main/java/com/egormoroz/schooly/ui/main/MainFragment.java
+++ app/src/main/java/com/egormoroz/schooly/ui/main/MainFragment.java
@@ -1,20 +1,19 @@
 package com.egormoroz.schooly.ui.main;
 
+import androidx.lifecycle.ViewModelProvider;
+
 import android.os.Bundle;
-import android.view.LayoutInflater;
-import android.view.View;
-import android.view.ViewGroup;
-import android.widget.ImageView;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
-import androidx.lifecycle.ViewModelProvider;
 
-import com.egormoroz.schooly.MainActivity;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
 import com.egormoroz.schooly.R;
 import com.google.android.material.appbar.AppBarLayout;
-import com.google.android.material.bottomnavigation.BottomNavigationView;
 
 public class MainFragment extends Fragment {
 
@@ -28,10 +27,8 @@ public class MainFragment extends Fragment {
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container,
                              @Nullable Bundle savedInstanceState) {
         View root = inflater.inflate(R.layout.fragment_main, container, false);
-        AppBarLayout abl = getActivity().findViewById(R.id.AppBarLayout);
+        AppBarLayout abl=getActivity().findViewById(R.id.AppBarLayout);
         abl.setVisibility(abl.VISIBLE);
-        BottomNavigationView bnv = getActivity().findViewById(R.id.bottomNavigationView);
-        bnv.setVisibility(bnv.VISIBLE);
         return root;
     }
 
@@ -39,27 +36,6 @@ public class MainFragment extends Fragment {
     public void onActivityCreated(@Nullable Bundle savedInstanceState) {
         super.onActivityCreated(savedInstanceState);
         mainViewModel = new ViewModelProvider(this).get(MainViewModel.class);
-    }
-
-    @Override
-    public void onViewCreated(@Nullable View view,@NonNull Bundle savedInstanceState){
-        super.onViewCreated(view, savedInstanceState);
-
-        View viewsubj=view.findViewById(R.id.subj);
-        viewsubj.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                ((MainActivity)getActivity()).setCurrentFragment(SubjectFragment.newInstance());
-            }
-        });
-
-        ImageView imageViewlib =view.findViewById(R.id.lib);
-        imageViewlib.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                ((MainActivity)getActivity()).setCurrentFragment(LibraryFragment.newInstance());
-            }
-        });
     }
 
 }
